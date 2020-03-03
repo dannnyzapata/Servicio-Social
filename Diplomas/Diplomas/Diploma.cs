c7dffaad8ff39f5dc39d4495967c6a5538fe7785
DannyZeta <a299474@uach.mx>
2020-03-03 15:55:13 -0700

    Proyecto

---
+++ Diplomas/Diplomas/Diploma.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace Diplomas
+{
+    public partial class Diplomad : Form
+    {
+        Conexion con = new Conexion();
+        public Diplomad(int folio)
+        {
+
+            InitializeComponent();
+
+            
+
+            for(int i=1; i<folio; i++)
+            {
+                lbNombre.Text = con.LabelName(i);
+                /*con.conectar();
+                lbFolio.Text = con.ejecutarQuery("select Folio FROM Registrados WHERE Folio= " + i).ToString();
+                con.desconectar();*/
+                lbFolio.Text = con.LabelFolio(i);
+
+
+                this.Show();
+                this.Hide();
+                using (Bitmap bmp = new Bitmap(this.Width, this.Height))
+                {
+                    this.DrawToBitmap(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height));
+                    bmp.Save(@"Diplomas/" + lbNombre.Text + ".bmp");
+
+                }
+            }
+
+           
+        }
+    }
+}
---
+++ Diplomas/Diplomas/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Diplomas
+{
+    public partial class Datos : Form
+    {
+        int i = 0;
+
+        Conexion con = new Conexion();
+        public Datos()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'alumnosDBDataSet.Registrados' table. You can move, or remove it, as needed.
+            this.registradosTableAdapter.Fill(this.alumnosDBDataSet.Registrados);
+
+        }
+
+        private void btActualizar_Click(object sender, EventArgs e)
+        {
+            con.conectar();
+
+            i++;
+            con.ejecutaTransaccion("insert into Registrados values ('" +
+                txtNombre.Text + "','"
+                + txtApellido1.Text + "','"
+                + txtApellido2.Text + "','"
+                 + txtCorreo.Text + "','"
+                + dtpFecha.Value.ToString("dd/MM/yyyy") + "')"
+                );
+            txtApellido1.Clear();
+            txtApellido2.Clear();
+            txtCorreo.Clear();
+            txtNombre.Clear();
+
+
+            /*foreach (DataGridViewRow row in dgvAlumnos.Rows)
+            {
+                con.ejecutaTransaccion("insert into Registrados values('NULL','" + row.Cells["Nombre"].Value.ToString() + "','" + row.Cells["Apellido1"].Value.ToString()
+                    + "','" + row.Cells["Apellido2"].Value.ToString() + "','" + row.Cells["Correo"].Value.ToString() + "','"
+                    + row.Cells["Apellido1"].Value.ToString() + "','" + row.Cells["Fecha"].Value.ToString() + "')");
+            }
+
+            */
+            con.desconectar();
+        }
+
+        private void btDiplomas_Click(object sender, EventArgs e)
+        {
+            Conexion con = new Conexion();
+            int pfolio;
+            //con.conectar();
+            //string folios; 
+            //folios = con.ejecutarQuery("select count(*) from Registrados").ToString();
+            // MessageBox.Show(folios);
+
+            pfolio = con.nAlumnos();
+            //MessageBox.Show(pfolio.ToString());
+
+            con.desconectar();
+            Diplomad pDiploma = new Diplomad(pfolio);
+            //pDiploma.ShowDialog();
+           
+
+        }
+    }
+}
